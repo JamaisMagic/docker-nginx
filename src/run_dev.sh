a9127c30ee162358e953b93b54c4207fc594890a
JamaisMagic <1350756140@qq.com>
2018-07-29 15:12:33 +0800

    update docker nginx settings.

---
+++ src/run_dev.sh
@@ -0,0 +1,17 @@
+if [ "$1" == "build" ]; then
+    docker-compose -f ./docker-compose.yml build
+elif [ "$1" == "up" ]; then
+    docker-compose -f ./docker-compose.yml up -d
+elif [ "$1" == "recreate" ]; then
+    docker-compose -f ./docker-compose.yml up -d --force-recreate
+elif [ "$1" == "restart" ]; then
+    docker-compose -f ./docker-compose.yml restart
+elif [ "$1" == "stop" ]; then
+    docker-compose -f ./docker-compose.yml stop
+elif [ "$1" == "t" ]; then
+    docker-compose -f ./docker-compose.yml exec nginx -t
+elif [ "$1" == "reload" ]; then
+    docker-compose -f ./docker-compose.yml exec nginx -s reload
+else
+    echo "Unexpected parameter: $1"
+fi
